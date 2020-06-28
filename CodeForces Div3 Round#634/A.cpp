6ae0db65a0d603abfcb33b4830790b1dcc6b4c59
noob-hu-yaar <48630662+noob-hu-yaar@users.noreply.github.com>
2020-06-29 04:30:16 +0530

    Add files via upload

---
+++ CodeForces Div3 Round#634/A.cpp
@@ -0,0 +1,33 @@
+
+
+#include <bits/stdc++.h> 
+#define ll long long
+#define ull unsigned long long
+#define endl '\n'
+#define pb push_back
+#define mp make_pair
+#define sp(x) fixed<<setprecision(x)
+#define p_q priority_queue
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+
+int t;
+int main()
+{
+    fast_io;
+
+    cin>>t;
+while(t--)
+{
+    ll n;
+    cin>>n;
+
+    if(n%2 != 0)
+    cout<<n/2<<endl;
+
+    else if(n%2 == 0)
+    cout<<(n-1)/2<<endl;
+}
+
+return 0;
+}
---
+++ CodeForces Div3 Round#634/B.cpp
@@ -0,0 +1,31 @@
+
+
+#include <bits/stdc++.h> 
+#define ll long long
+#define ull unsigned long long
+#define endl '\n'
+#define pb push_back
+#define mp make_pair
+#define sp(x) fixed<<setprecision(x)
+#define p_q priority_queue
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+
+int t;
+int main()
+{
+    fast_io;
+
+    cin>>t;
+while(t--)
+{
+    ll n,a,b;
+    cin>>n>>a>>b;//>>c;
+
+    for(ll i=0;i<n;i++)
+    cout<<(char)('a' + i%b);//
+    cout<<endl;
+}
+
+return 0;
+}
