8100f32b938d3620494a5bfbffa7b06bd49e67d1
noob-hu-yaar <48630662+noob-hu-yaar@users.noreply.github.com>
2020-07-11 23:57:37 +0530

    Add files via upload

---
+++ CodeForces Div2 #655/A.cpp
@@ -0,0 +1,35 @@
+
+
+#include <bits/stdc++.h> 
+#define ll long long
+#define ull unsigned long long
+#define endl '\n'
+#define pb push_back
+#define mp make_pair
+#define sp(x) fixed<<setprecision(x)
+#define p_q priority_queue
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+
+ll t;
+int main()
+{
+    fast_io;
+
+    cin>>t;
+while(t--)
+{
+    ll n;
+    cin>>n;
+    
+   // ll i = 1;
+    while(n--)
+    {
+        cout<<1<<" ";
+        //i += 2;
+    }
+    cout<<endl;
+}
+
+return 0;
+}
